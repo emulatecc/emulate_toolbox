10235e2f2597b1e2b03f3a7686251b1f8784a560
Mathias Storm <mathias@mstorm.me>
2015-10-09 22:56:54 +0200

    Adding NotificationWindow and refernce for Framework

---
+++ Toolbox.Forms/Dialoges/NotificationBox.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+
+
+namespace Toolbox.Forms.Dialoges
+{
+    /// <summary>
+    /// Interaktionslogik für NotificationBox.xaml
+    /// </summary>
+    public partial class NotificationBox : Window
+    {
+        //static  Execute(string message, );
+
+
+        public NotificationBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
