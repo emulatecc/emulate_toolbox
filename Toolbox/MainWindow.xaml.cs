2cfeedf337709d27e8d3945a53f7fb4a84ee50a0
Mathias Storm <mathias@mstorm.me>
2015-10-09 18:15:43 +0200

    Cleaning up projects, creating main project

--- Toolbox/MainWindow.xaml.cs
+++ Toolbox/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Toolbox.Forms
+namespace Toolbox
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
